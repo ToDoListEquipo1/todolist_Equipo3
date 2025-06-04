e900a438e31380f2a3035cdda8132f90de236054
Tatiananay <nayeli.leiva@epn.edu.ec>
2025-06-04 17:01:10 -0500

    modulo de servicio tareas y usuarios hecho

---
+++ src/main/java/example/ToDoApp/servicio/TareaServiceException.java
@@ -0,0 +1,8 @@
+package example.ToDoApp.servicio;
+
+public class TareaServiceException extends RuntimeException {
+
+    public TareaServiceException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/example/ToDoApp/servicio/UsuarioServiceException.java
@@ -0,0 +1,8 @@
+package example.ToDoApp.servicio;
+
+public class UsuarioServiceException extends RuntimeException {
+
+    public UsuarioServiceException(String message) {
+        super(message);
+    }
+}
